4a83a05b56b27011fb1351215598b9ad294eb391
Mchtsz <mhatland206@gmail.com>
2023-09-09 18:54:45 +0200

    Alt funker

--- collection.js
+++ collection.js
@@ -1,28 +1,59 @@
-const fornavn = document.getElementById("fornavn");
-const etternavn = document.getElementById("etternavn");
-const adresse = document.getElementById("adresse");
-
-const postnr = document.getElementById("postnummer");
-const poststed = document.getElementById("poststed");
-const telefon = document.getElementById("telefon");
-const submitBtn = document.getElementById("submit");
-
-const infoText = document.getElementById("infoText");
-
+const ErrorText = document.getElementById("ErrorText");
+const InfoText = document.getElementById("InfoText");
+const form = document.getElementById("form");
 let kundeListe = [{}];
 
-submitBtn.addEventListener("click", () => {
-  const kunde = {
-    fornavn: fornavn.value,
-    etternavn: etternavn.value,
-    adresse: adresse.value,
-    postnr: postnr.value,
-    poststed: poststed.value,
-    telefon: telefon.value,
-  };
+function formatFormData(formData, ...args) {
+  const data = {};
 
-  kundeListe.push(kunde);
-  console.log(kundeListe);
+  for (let key of args) {
+    data[key] = formData.get(key);
+  }
 
-  infoText.innerText = "Kunde lagt til";
+  return data;
+}
+
+function returnErr(message) {
+  ErrorText.innerText = message;
+  return false;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const data = formatFormData(
+    formData,
+    "fornavn",
+    "etternavn",
+    "adresse",
+    "postnummer",
+    "poststed",
+    "telefon",
+    "passord"
+  );
+
+  // console.log(data);
+  let hasUpperCase = /[A-Z]/.test(data.passord);
+  let hasLowerCase = /[a-z]/.test(data.passord);
+  let hasNumber = /\d/.test(data.passord);
+  let hasSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(
+    data.passord
+  );
+
+  if (data.passord.length < 10) {
+    return returnErr("Passordet må være minst 10 tegn langt");
+  } else if (!hasUpperCase && hasLowerCase) {
+    return returnErr("Passordet må ha minst en stor og en liten bokstav!");
+  } else if (!hasNumber) {
+    return returnErr("Passordet må ha minst et tall!");
+  } else if (!hasSpecialCharacter) {
+    return returnErr("Passordet må ha minst et spesialtegn!");
+  } else {
+    kundeListe.push(data);
+    console.log(kundeListe);
+    InfoText.innerText = `Du heter ${data.fornavn} ${data.etternavn} og bor i ${data.adresse}, ${data.postnummer} ${data.poststed}. Telefonnummeret ditt er ${data.telefon}. Vi har solgt alle personopplysningene dine til facebook. Ha en fin dag!`;
+    returnErr("");
+  }
 });
